8028b38a88053cec5a29a3bd62da4d31f76ad3cd
CyrilenBlu <lukepetzerint2@gmail.com>
2019-07-20 23:00:43 +0200

    Added dockerfile and changed tomcat to provided when running inside container.

--- restful-layer/src/main/java/blu/springkub/restfullayer/RestfulLayerApplication.java
+++ restful-layer/src/main/java/blu/springkub/restfullayer/RestfulLayerApplication.java
@@ -3,12 +3,19 @@ package blu.springkub.restfullayer;
 import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import java.sql.SQLException;
 
 @SpringBootApplication
-public class RestfulLayerApplication {
+public class RestfulLayerApplication  extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RestfulLayerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RestfulLayerApplication.class, args);
@@ -17,7 +24,7 @@ public class RestfulLayerApplication {
     @Bean(initMethod = "start", destroyMethod = "stop")
     public Server inMemoryH2DatabaseServer() throws SQLException {
         return Server.createTcpServer(
-                "-tcp", "-tcpAllowOthers", "-tcpPort", "9090"
+                "-tcp", "-tcpAllowOthers", "-tcpPort", "8090"
         );
     }
 
--- web-layer/src/main/java/blu/springkub/weblayer/WebLayerApplication.java
+++ web-layer/src/main/java/blu/springkub/weblayer/WebLayerApplication.java
@@ -2,9 +2,16 @@ package blu.springkub.weblayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebLayerApplication {
+public class WebLayerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebLayerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WebLayerApplication.class, args);
