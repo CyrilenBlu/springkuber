13a483c4225409992dc593c2999cacb54d77634e
BuildTools <unconfigured@null.spigotmc.org>
2019-07-20 23:57:49 +0200

    Fixed test for packaging.

--- web-layer/src/test/java/blu/springkub/weblayer/WebLayerApplicationTests.java
+++ web-layer/src/test/java/blu/springkub/weblayer/WebLayerApplicationTests.java
@@ -3,11 +3,22 @@ package blu.springkub.weblayer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(
+        properties = {
+                "spring.datasource.url = jdbc:h2:mem:testdb"
+        }
+        )
 public class WebLayerApplicationTests {
+
+//    # Old
+//    static {
+//        System.setProperty("spring.datasource.url", "jdbc:h2:mem:testdb");
+//    }
 
     @Test
     public void contextLoads() {
